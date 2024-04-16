75cbd5819c9e38cffc89d976331599a557ba4fcc
rithub <ritamcharan232001@gmail.com>
2024-04-16 11:34:15 +0530

    initial commit

---
+++ src/main/java/com/rithub/email/SendEmailApp.java
@@ -0,0 +1,13 @@
+package com.rithub.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendEmailApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendEmailApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/rithub/email/SendEmailAppTests.java
@@ -0,0 +1,13 @@
+package com.rithub.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendEmailAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
